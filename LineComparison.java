945313b24395564d0cb2d1e6154651961cd9cdea
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-10-06 13:48:51 +0530

    Checking two lines are equal or not

--- LineComparison.java
+++ LineComparison.java
@@ -9,9 +9,10 @@ public class LineComparison {
     public  double pointX1;
     public double pointY2;
     public double pointY1;
-    public double distance;
+    public double line1;
+    public double line2;
 
-    public void getUserInput(){
+    public void getUserInputForLine(){
         Scanner scanInput = new Scanner(System.in);
         System.out.print("Enter line X1 coordinate : ");
         pointX1 = scanInput.nextDouble();
@@ -23,15 +24,27 @@ public class LineComparison {
         pointY2 = scanInput.nextDouble();
     }
 
-    public double lengthOfLine() {
+
+    public double lengthOfLine1() {
         // using Math.pow and Math.sqrt method
-        distance=Math.sqrt(
+        line1=Math.sqrt(
                 (Math.pow((pointX2-pointX1), 2))
                         +
                         (Math.pow(pointY2-pointY1, 2))
         );
-        return distance;
+        return line1;
 
     }
+    public double lengthOfLine2() {
+        // using Math.pow and Math.sqrt method
+        line2=Math.sqrt(
+                (Math.pow((pointX2-pointX1), 2))
+                        +
+                        (Math.pow(pointY2-pointY1, 2))
+        );
+        return line2;
+
+    }
+
 
 }
--- LineComparisonComputation.java
+++ LineComparisonComputation.java
@@ -4,7 +4,19 @@ public class LineComparisonComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Comparison computation program");
         LineComparison calculateLength = new LineComparison();
-        calculateLength.getUserInput();
-        System.out.print("length of line is : "+calculateLength.lengthOfLine());
+        calculateLength.getUserInputForLine();
+        System.out.println("length of line1 is : "+calculateLength.lengthOfLine1());
+        calculateLength.getUserInputForLine();
+        System.out.println("length of line2 is : "+calculateLength.lengthOfLine2());
+        String getLine1=calculateLength.line1+"";
+        String getLine2=calculateLength.line2+"";
+
+        boolean checkEquality = getLine1.equals(getLine2);
+
+        if(checkEquality) {
+            System.out.print("both line length are equal");
+        }else {
+            System.out.print("both line length are different");
+        }
     }
 }
