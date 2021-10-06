be0c8bcb9cd08ce144ec8112abb769e77e31c785
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-10-06 13:24:08 +0530

    welcome to line comparison computation program

---
+++ LineComparisonComputation.java
@@ -0,0 +1,8 @@
+package com.company1.LineComparisonComputationUsingOOps;
+
+public class LineComparisonComputation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Comparison computation program");
+        
+    }
+}
